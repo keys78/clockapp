07b3fcf0ade6ff37442bf55df3aa53e54cde2415
unknown <rahzy24@gmail.com>
2021-06-06 16:39:16 +0100

    testing stage 1

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,115 @@
 <template>
 
+  <div id="heroic" class="w-full h-screen">
+    <div class="py-12 lg:px-24 px-8">
+        <div class="flex items-start gap-6 h-10 lg:w-7/12 w-full ">
+          <div class="lg:w-7/12 sm:w-10/12 w-full animate__animated animate__slideInLeft  animate__faster">
+            <p class="text-white" v-if="quotesContent"> {{ quotesContent }} </p>
+            <p class="text-white font-bold italic mt-4" v-if="quotesAuthor">- {{ quotesAuthor }}</p>
+          </div>
+          <div class="lg:w-3/12 sm:w-5/12 w-2/12">
+            <button class="focus:outline-none" @click="refreshQuotes"><img id="refresh-icon" src="./assets/desktop/icon-refresh.svg"/></button>
+          </div>
+        </div>
 
-  <div>
-    <Showmore/>
+        <div class="pt-80">
+          <div class="flex items-center gap-4">
+              <!-- <div v-if="galaxyIcon"> {{ galaxyIcon }}</div> -->
+              <!-- <img :src="galaxyIcon"/> -->
+              <img id="sun" src="./assets/desktop/icon-sun.svg"/>
+              <img id="moon" src="./assets/desktop/icon-moon.svg"/>
+              <h1 v-if="greetings" class="tracking-wider md:text-3xl sm:text-2xl text-base text-white"> {{ greetings }}, IT'S CURRENTLY </h1>
+          </div>
+          <h1 v-if="timestamp" class="md:text-9xl sm:text-8xl text-6xl py-4 text-white font-bold"> {{ timestamp }} <span v-if="abbreviation" class="text-xl font-medium"> {{ abbreviation }} </span></h1>
+
+          <h2 class="md:text-2xl sm:text-xl text-base font-semibold text-white uppercase tracking-widest" v-if="timezone">IN {{ timezone }} </h2>
+        </div>
+         <button @click="openNav" class="absolute sm:bottom-20 bottom-8 sm:right-24 right-12 text-black bg-white py-1 myhov px-4 flex items-center gap-3 focus:outline-none cursor-pointer rounded-2xl" ><span>More</span> <img  id="showmore-Btn" class="w-7" src="./assets/desktop/icon-arrow-up.svg"/></button>
+    </div>
+
+       
+        <div id="myBottomNav" class="bottomNav h-2/4 bg-black opacity-90 lg:px-24 px-8 ">
+
+          <button @click="closeNav"  id="showmore-Btn" class="absolute sm:bottom-20 bottom-8 sm:right-24 right-12 bg-white text-black py-1 myhov px-4 flex items-center gap-3 focus:outline-none cursor-pointer rounded-2xl"><span>Lesss </span> <img  class="w-7  iconio" src="./assets/desktop/icon-arrow-up.svg"/></button>
+
+          <div class="flex justify-between items-start">
+
+              <div >
+                <div class="py-16 lg:w-6/12 sm:w-8/12 w-full">
+                  <p class="sm:text-base text-xs uppercase tracking-wide text-gray-200">Current Timezone</p>
+                  <p v-if="currentTimezone" class="uppercase lg:text-5xl sm:text-4xl text-2xl font-semibold text-gray-200">{{ currentTimezone }}</p>
+                </div>
+                
+                <div>
+                  <p class="sm:text-base text-xs uppercase tracking-wide text-gray-200">Day of the year</p>
+                  <p v-if="dayofyear" class="uppercase lg:text-5xl sm:text-4xl text-2xl font-semibold text-gray-200">{{ dayofyear }}</p>
+                </div>
+              </div>
+
+              <div class="sm:border-l-2  border-none border-gray-200 w-6/12">
+                  <div class="py-16 pl-8">
+                    <p class="sm:text-base text-xs uppercase tracking-wide text-gray-200">Day of the week</p>
+                    <p v-if="dayofweek" class="uppercase lg:text-5xl sm:text-4xl text-2xl font-semibold text-gray-200">{{ dayofweek }}</p>
+                  </div>
+
+                  <div class="pl-8">
+                    <p class="sm:text-base text-xs uppercase tracking-wide text-gray-200">week number</p>
+                    <p v-if="weekNumber" class="uppercase lg:text-5xl sm:text-4xl text-2xl font-semibold text-gray-200">{{ weekNumber }}</p>
+                  </div>
+              </div>
+
+          </div>
+
+        </div>
+
   </div>
-
-
-
-
-
-
-  <!-- <div id="heroic" class="w-full md:h-screen h-auto p-12" >
-    <div class="">
-      <p class="text-white" v-if="quotesContent"> {{ quotesContent }} </p>
-      <p class="text-white" v-if="quotesAuthor"> {{ quotesAuthor }}</p>
-      <button class="focus:outline-none" @click="refreshQuotes"><img id="refresh-icon" src="./assets/desktop/icon-refresh.svg"/></button>
-    </div>
-
-    <div class="pt-96">
-      <div>
-        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M11.78 19.417c.594 0 1.083.449 1.146 1.026l.006.125v1.842a1.152 1.152 0 01-2.296.125l-.007-.125v-1.842c0-.636.516-1.151 1.152-1.151zM6.382 17.18a1.15 1.15 0 01.09 1.527l-.09.1-1.302 1.303a1.152 1.152 0 01-1.718-1.528l.09-.1 1.302-1.302a1.15 1.15 0 011.628 0zm12.427 0l1.303 1.303a1.15 1.15 0 11-1.628 1.627L17.18 18.81a1.15 1.15 0 111.628-1.628zM11.781 5.879a5.908 5.908 0 015.901 5.902 5.908 5.908 0 01-5.901 5.902 5.908 5.908 0 01-5.902-5.902 5.908 5.908 0 015.902-5.902zm10.63 4.75a1.151 1.151 0 110 2.303h-1.843a1.151 1.151 0 110-2.303h1.842zm-19.418 0a1.151 1.151 0 01.126 2.296l-.125.007H1.15a1.151 1.151 0 01-.125-2.296l.125-.007h1.842zm1.985-7.268l.1.09 1.303 1.302a1.151 1.151 0 01-1.528 1.718l-.1-.09L3.45 5.08A1.15 1.15 0 014.978 3.36zm15.133.09c.45.449.45 1.178 0 1.628L18.808 6.38a1.151 1.151 0 11-1.628-1.628l1.303-1.303c.449-.449 1.178-.449 1.628 0zM11.781 0c.636 0 1.151.515 1.151 1.151v1.843a1.152 1.152 0 01-2.303 0V1.15C10.63.515 11.145 0 11.781 0z" fill="#FFF" fill-rule="nonzero"/></svg>
-      </div>
-      <h1 v-if="greetings" class="text-3xl text-white"> {{ greetings }}, IT'S CURRENTLY </h1>
-      <h1 v-if="timestamp" class="text-9xl text-white font-bold"> {{ timestamp }} <span v-if="abbreviation" class="text-base"> {{ abbreviation }} </span></h1>
-
-      <div class="flex justify-between">
-      <h2 class="text-2xl text-bold text-white uppercase" v-if="timezone">IN {{ timezone }} </h2>
-        <button class="focus:outline-none" @click="showMoreLess"><img id="refresh-icon" src="./assets/desktop/icon-arrow-up.svg"/></button>
-      </div>
-    </div>
-
-
-     -->
 </template>
 
-<script>
-import Showmore from './components/Showmore.vue'
-
+// <script>
+//   import Sun from "./assets/desktop/icon-sun.svg"
+//   import Moon from "./assets/desktop/icon-moon.svg"
 export default {
+  
   name: 'App',
   components: {
-    Showmore
+  
   },
   data() {
     return {
-      quotesContent:'',
-      quotesAuthor: '',
+      quotesContent:'The universe is transformation; our life is what our thoughts make it.',
+      quotesAuthor: 'Em_codes',
       greetings:'',
       timestamp:'',
       abbreviation:'',
       timezone:'',
+      // galaxyIcon:''
+      currentTimezone:'',
+      dayofyear:'',
+      dayofweek:'',
+      weekNumber:'',
+     
     }
   },
   
   methods: {
+
+     openNav() {
+                document.getElementById("myBottomNav").style.height = "50%";
+                document.getElementById("heroic").style.marginBottom = "350px";
+                document.body.style.background = "black"
+              },
+
+              closeNav() {
+                document.getElementById("myBottomNav").style.height = "0";
+                document.getElementById("heroic").style.marginBottom= "0";
+              },
+
+
+
+
+
+
       refreshQuotes() {
-          this.quotesContent.length > 0 ? document.getElementById('refresh-icon').style.animation = "rotation 1s linear" : document.getElementById('refresh-icon').style.animation = "rotation 1s linear"
          fetch('https://api.quotable.io/random')
             .then(res => {
               
@@ -67,7 +121,7 @@ export default {
             this.quotesAuthor = data.author
             
             })
-           
+         
       },
 
        getNow: function() {
@@ -78,7 +132,14 @@ export default {
 
                     const myHour = today.getHours()
                     myHour >= 12 ? document.getElementById('heroic').style.backgroundImage = "url('https://images.pexels.com/photos/920534/pexels-photo-920534.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')" : document.getElementById('heroic').style.backgroundImage = "url('https://images.pexels.com/photos/1024981/pexels-photo-1024981.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')"
-                    myHour >= 12 ? this.greetings = "GOOD EVENING" : this.greetings = "GOOD MORNING"
+                    myHour >= 12 ? this.greetings = "GOOD EVENING" : this.greetings = "GOOD MORNING";
+                    if(myHour >= 12){
+                       document.getElementById('sun').style.display = 'none';
+                       document.getElementById('moon').style.display = 'block';
+                    }else{
+                        document.getElementById('sun').style.display = 'block';
+                       document.getElementById('moon').style.display = 'none';
+                    }
                 }
 
   },
@@ -94,14 +155,16 @@ export default {
                     return res.json();
                     })
                 .then(data => {
-                console.log(data.abbreviation)
-                // this.greetings = 'GOOD MORNINING, ITS CURRENTLY'
+                console.log(data)
                 this.abbreviation = data.abbreviation
+                this.currentTimezone = data.timezone
                 this.timezone = data.timezone
-                this.currenttime = data.datetime
-              
+                this.dayofyear = data.day_of_year
+                this.dayofweek = data.day_of_week
+                this.weekNumber = data.week_number
                 })
-          // this.timestamp.value >= 12 ? document.getElementById('heroic').style.background = 'red' : document.getElementById('heroic').style.backgroundImage = 'green'
+       
+               
           
       },
   
@@ -120,38 +183,38 @@ export default {
   padding: 0;
   margin:0;
 }
-
-.hero-bg{
-   background-image: url('https://images.pexels.com/photos/1024981/pexels-photo-1024981.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-  background-repeat: no-repeat;
-  background-position: cover;
-  width:100%;
+#refresh-icon:hover{
+  transform: rotate(360deg);
+  transition: 1.2s ease-in-out;
 }
 
 
-
-@-webkit-keyframes rotation {
-		from {
-				-webkit-transform: rotate(0deg);
-		}
-		to {
-				-webkit-transform: rotate(359deg);
-		}
+/* POPUP */
+#showmore-Btn:hover .iconio{
+   transform: rotate(180deg);
+   transition: 0.3s ease-in-out;
 }
-@keyframes rotation {
-		from {
-				-webkit-transform: rotate(0deg);
-		}
-		to {
-				-webkit-transform: rotate(359deg);
-		}
+.iconio{
+    transition: 0.3s ease-in-out;
 }
 
-/* .heroic{
-  background-image: url('https://images.pexels.com/photos/920534/pexels-photo-920534.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-  background-repeat: no-repeat;
-  background-position: cover;
-  width:100%;
-} */
+
+.bottomNav {
+  width:100vw;
+  height: 0;
+  position: fixed;
+  bottom: 0;
+  z-index: 1;
+  overflow: hidden;
+  transition: 0.5s;
+}
+
+.myhov:hover{
+  transform: scale(1.1);
+  transition: 0.3s ease-in-out;
+}
+.myhov{
+  transition: 0.3s ease-in-out;
+}
 
 </style>
