bb0c4474b0501a0e50465d3bcbc655e91bcb0abc
unknown <rahzy24@gmail.com>
2021-06-07 23:09:08 +0100

    testing stage 3

--- src/App.vue
+++ src/App.vue
@@ -150,7 +150,7 @@ export default {
                   
       mounted() 
       {
-        fetch('https://worldtimeapi.org/api/ip')
+        fetch('http://worldtimeapi.org/api/ip')
                 .then(res => {
                     return res.json();
                     })
